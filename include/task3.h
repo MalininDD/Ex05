a100c72989a6d8007cc95b208930659a81581ce8
MalininDD <54958481+MalininDD@users.noreply.github.com>
2020-11-05 17:47:40 +0300

    Add files via upload

--- include/task3.h
+++ include/task3.h
@@ -1,6 +1,6 @@
 #include <iostream>
 template <typename T, size_t sizeofmas>
-T minArr(T2 (&a)[sizeofmas]){
+T minArr(T (&a)[sizeofmas]){
     T minn = 12312312;
     for (int i = 0; i  < sizeofmas; i++) {
         if(a[i] < minn)
