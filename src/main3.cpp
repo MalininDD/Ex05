caf63ae0feaf551390c9726f68d51e709c41d118
MalininDD <54958481+MalininDD@users.noreply.github.com>
2020-11-05 17:36:00 +0300

    Add files via upload

---
+++ src/main3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "task1.h"
+#include "task2.h"
+#include "task3.h"
+
+
+//
+//template<typename T1>
+//double averageArr(int n, T1* mass){
+//    double sum = 0;
+//    for (int i = 0; i  < n; i++) {
+//        sum += (double)mass[i];
+//    }
+//    return sum/n;
+//}
+//template <typename T2, size_t sizeofmas>
+//
+//T2 minArr(T2 (&a)[sizeofmas]){
+//    T2 minn = 12312312;
+//    for (int i = 0; i  < sizeofmas; i++) {
+//        if(a[i] < minn)
+//            minn = a[i];
+//    }
+//    return minn;
+//}
+
+int main() {
+//    const size_t n = 6;
+//    int val[n]{4, 3, 2, 2, 2, -1};
+//    std::cout << minArr(val) << std::endl;
+    return 0;
+}
