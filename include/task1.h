22281c341ee6568659b678cfe86aa8add6badcb8
MalininDD <54958481+MalininDD@users.noreply.github.com>
2020-11-07 16:39:34 +0300

    Add files via upload

--- include/task1.h
+++ include/task1.h
@@ -1,23 +1,11 @@
 #include <iostream>
 
-bool comparestr(const char* a,const char* b){
-    for (int i = 0; i < strlen((const char*)a); i++) {
-        if((int)a[i] - (int)b[i] < 0)
-            return true;
-        else if((int)a[i] - (int)b[i] > 0)
-            return false;
-    }
-    return false;
+template <typename T>
+
+bool less(T a, T b) {
+    return a < b;
 }
 
-template<typename T>
-bool less(T a,T b){
-    if(typeid(T) == typeid(const char*)){
-        return comparestr((const char*)a,(const char*)b);
-        
-    }
-    if(a < b)
-        return true;
-    else
-        return false;
+template<> bool less <const char*>(const char* a, const char* b) {
+    return strcmp(a, b) < 0;
 }
